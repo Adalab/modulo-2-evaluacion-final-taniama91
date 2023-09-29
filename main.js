fa9e977d39330b9ad9adc503b8f03b2edee67808
Tania <taniie_bcn@hotmail.es>
2023-09-29 08:52:24 +0200

    add class & start js

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//CONSTANTES (FORM/SECTION)
+
+const inputSearch = document.querySelector('.js-input-search');
+const btnSearch = document.querySelector('.js-btn-Search');
+const listFav = document.querySelector('.js-list-fav');
+const listSearch =document.querySelector('.js-list-search');
+
+
