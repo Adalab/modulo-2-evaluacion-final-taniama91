58597cc66f5c757c9bc66c900a34827c9ed8f32e
Tania <taniie_bcn@hotmail.es>
2023-09-30 14:18:04 +0200

    added id

--- main.js
+++ main.js
@@ -14,15 +14,14 @@ const listSearch =document.querySelector('.js-list-search'); //lista con la busq
 let seriesSearch = []; 
 let seriesFav = [];
 
-
+//PEDIR INFO A LA API
 function getApiInfo () {
     const valueInput = inputSearch.value;    
-        //URL
+        //URL API + VALOR INTRODUCIDO EN EL INPUT
     const url =`//api.tvmaze.com/search/shows?q=${valueInput}`
     fetch(url)
     .then((response) => response.json())
     .then(dataApi => {       
-        //console.log(dataApi);
         //pintar un listado
         seriesSearch = dataApi;   
         renderSeriesList (seriesSearch);
@@ -32,9 +31,8 @@ function getApiInfo () {
 //PINTA ESTRUCTURA SERIE
 function renderSeries(serie){
     let html="";
-    
-    html+=`<ul class="serie">`;
-    
+    //añade el id de cada serie a cada ul
+    html+=`<ul id=${serie.show.id} class="serie js-serie">`; 
     //CONDICIONAL QUE ME DEVUELVE LA FOTO Y SI NO TIENE ME PONE UNA IMAGEN DE RELLENO
     if (serie.show.image !== null){
         html+=`<li>
@@ -57,14 +55,41 @@ function renderSeriesList (listSeries){
     for (const serie of listSeries) {
         listSearch.innerHTML+= renderSeries(serie)
     }
+    /*llamo a la funcion addEventToSerie para que me pinte las series (lo hago aqui porque es donde me pinta las series y si lo pongo debajo del evento del boton de buscar me sale vacio, ya que EL FETCH TODAVIA NO ME HA DEVUELTO LA INFO Y aun no tengo pintadas las series)*/
+    addEventToSerie();
 }
 
+//FUNCIÓN SOBRE EL BOTÓN DE BUSCAR
 function handleClickSearch(event) {
     event.preventDefault();
     //devuelveme la info de API 
     getApiInfo ();
 };
 
+//FUNCION SOBRE AÑADIR A FAVORITOS
+function handleClickFav(event){
+//la ul es quien escucha el evento ya que he hecho un qsAll sobre la ul
+console.log(event.currentTarget.id);
+const idSerieCliked = parseInt(event.currentTarget.id);
+//buscar la serie por id para saber en cual he dado click
+const foundSerie = seriesSearch.find(item => item.show.id === idSerieCliked); 
+console.log(foundSerie);
+}
+
+/*FUNCION PARA OBTENER TODAS LAS SERIES QUE HE BUSCADO (qsAll porque no se cuantas tengo en total, ya que la info viene del servidor)*/
+function addEventToSerie(){
+const allSeries = document.querySelectorAll(".js-serie");
+console.log(allSeries);
+//añadir la funcion a cada una de las series a las que haga click
+for (const item of allSeries) {
+    item.addEventListener('click', handleClickFav)
+}
+}
+
+//FUNCION PARA AÑADIR LAS SERIES A FAVORITAS
+
 
 //EVENTO SOBRE BOTÓN DE BUSCAR
 btnSearch.addEventListener('click', handleClickSearch);
+
+
